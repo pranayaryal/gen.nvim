e742928bf29f20aff3eaaed109acb37ee6278eec
Pranay Aryal <drpranayaryal@gmail.com>
2024-09-08 10:18:52 -0400

    Update init.lua

--- lua/gen/init.lua
+++ lua/gen/init.lua
@@ -460,6 +460,7 @@ M.prompts = prompts
 local function select_prompt(cb)
     local promptKeys = {}
     for key, _ in pairs(M.prompts) do table.insert(promptKeys, key) end
+    table.sort(promptKeys)
     vim.ui.select(promptKeys, {
         prompt = "Prompt:",
         format_item = function(item)
